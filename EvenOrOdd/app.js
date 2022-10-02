4bdd7c39d0ded8e97673fed73b0606570b833176
hngoldman995 <hannahgoldman995@gmail.com>
2022-10-02 19:31:32 -0400

    today's final practice problem

--- EvenOrOdd/app.js
+++ EvenOrOdd/app.js
@@ -2,15 +2,15 @@
 Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 function even_or_odd(number) {
-    //     if (number % 2 === 0) {
-    //         return 'even';
-    //     } else {
-    //         return 'odd'
-    //     }
-    // }
-    // return number % 2 === 0 ? 'even' : 'odd';
-
+    if (number % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd'
+    }
 }
+// return number % 2 === 0 ? 'even' : 'odd';
+
+
 
 
 
