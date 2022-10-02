1fb70476de93857c68fe63aa1c37aca4fcd96f77
hngoldman995 <hannahgoldman995@gmail.com>
2022-10-02 18:58:38 -0400

    another practice problem

---
+++ EvenOrOdd/app.js
@@ -0,0 +1,21 @@
+/*
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+function even_or_odd(number) {
+    //     if (number % 2 === 0) {
+    //         return 'even';
+    //     } else {
+    //         return 'odd'
+    //     }
+    // }
+    // return number % 2 === 0 ? 'even' : 'odd';
+
+}
+
+
+
+
+console.log(even_or_odd(4))
+console.log(even_or_odd(13))
+
+
