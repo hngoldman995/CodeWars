00c68facaa515b467981a2a9dd75e548d95bd23d
hngoldman995 <hannahgoldman995@gmail.com>
2023-01-17 19:44:39 -0500

    Today's Challenges

---
+++ CalculateAv/app.js
@@ -0,0 +1,13 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    
+    let sum = array.reduce((acc, c) => acc + c, 0)
+    if (array.length === 0){
+        return 0
+    }else{
+        return sum / array.length 
+    }
+  }
---
+++ RemoveMarks/app.js
@@ -0,0 +1,6 @@
+DESCRIPTION:
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.replaceAll('!', '');
+  }
