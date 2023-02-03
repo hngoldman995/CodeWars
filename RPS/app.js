a2b40b0929948bec14903cbcf6547decbb12e75c
hngoldman995 <hannahgoldman995@gmail.com>
2023-02-02 23:56:17 -0500

    Today's Challenge

--- RPS/app.js
+++ RPS/app.js
@@ -26,6 +26,7 @@ const rpss = (p1, p2) => {
     else if (p1 == 'scissors' && p2 == 'paper')
         return 'Player 1 won!'
     else if (p1 == 'paper' && p2 == 'rock')
+    
         return 'Player 1 won!'
     else
         return 'Player 2 won!';
