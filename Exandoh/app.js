6abc8321ba2f8ea3eb496648b5cc62ee9aa66b60
hngoldman995 <hannahgoldman995@gmail.com>
2023-02-06 16:37:21 -0500

    Today's Challenge

---
+++ Exandoh/app.js
@@ -0,0 +1,25 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+    let lengOfX = str.replace(/[xX]/g).length
+    let lengOfO = str.replace(/[oO]/g).length
+    if ( lengOfX === lengOfO){ 
+        return true 
+    }else{
+        return false
+    }
+}
+
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+  }
