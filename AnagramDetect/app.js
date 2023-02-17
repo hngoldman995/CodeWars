f76ca15e3ea7c8b94448f6fa2586e2fa3eb571f4
hngoldman995 <hannahgoldman995@gmail.com>
2023-02-16 20:13:36 -0500

    Today's Challenge

--- AnagramDetect/app.js
+++ AnagramDetect/app.js
@@ -11,8 +11,6 @@
 // "Buckethead" is an anagram of "DeathCubeK"
 
 function isAnagram(test, original) {
-    
     return test.toLowerCase().split("").sort().join("") === 
     original.toLowerCase().split("").sort().join("");
-    
 }
