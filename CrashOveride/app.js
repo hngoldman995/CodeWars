cc1d5de2050ea2d1e1335881f4fc1748a6ddf5d1
hngoldman995 <hannahgoldman995@gmail.com>
2023-03-25 23:27:11 -0400

    Today's Challenge

--- CrashOveride/app.js
+++ CrashOveride/app.js
@@ -13,6 +13,7 @@
 function aliasGen(first,last){
     let auxFirst = first[0].toUpperCase( )
     let auxLast = last[0].toUpperCase( )
+    
     if(firstName[auxFirst] && surname[auxLast]) {
       return firstName[auxFirst] + ' ' + surname[auxLast]
     } else {
