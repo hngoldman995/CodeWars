f44d3b62883f16bf3ee8f95cadb1027cf80d0e43
hngoldman995 <hannahgoldman995@gmail.com>
2023-06-01 20:17:02 -0400

    Today's Challenge

--- Printer/main.js
+++ Printer/main.js
@@ -15,6 +15,8 @@
 // s="aaaxbbbbyyhwawiwjjjwwm"
 // printer_error(s) => "8/22"
 
+
+
 function printerError(s) {
     // your code
     var count = 0;
@@ -23,5 +25,5 @@ function printerError(s) {
         count++;
       }
     }
-    return count+"/"+s.length;
+    return count+"/"+s.legngth;
 }
